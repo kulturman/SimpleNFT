bd2b209f37e8decf23b1ee5fb75a3d7718f9e0f0
kulturman <arnaudbakyono@gmail.com>
2025-03-11 22:39:37 +0000

    Add test for approve

---
+++ test/SimpleNFCApprove.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {SimpleNFC, IERC721} from "../src/SimpleNFC.sol";
+
+contract SimpleNCF is Test {
+    SimpleNFC public simpleNFC;
+
+    function setUp() public {
+        simpleNFC = new SimpleNFC();
+        simpleNFC.generateToken();
+    }
+
+    function testApprovesSuccessfully() public {
+        uint256 tokenId = 1;
+        //Token is not approved by default
+        assertEq(simpleNFC.getApproved(tokenId), address(0));
+        address contractOwner = address(this);
+
+        vm.expectEmit();
+        emit IERC721.Approval(contractOwner, contractOwner, tokenId);
+        simpleNFC.approve(contractOwner, tokenId);
+        assertEq(simpleNFC.getApproved(tokenId), contractOwner);
+    }
+}
